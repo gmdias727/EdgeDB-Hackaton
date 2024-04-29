5f1f3ab31081958e9fe20a3b54b016403ee8c915
Gabriel Mazieri <gabrieldias7200@gmail.com>
2024-04-29 19:07:27 -0300

    wip

--- README.md
+++ README.md
@@ -30,6 +30,9 @@ go install github.com/edgedb/edgedb-go/cmd/edgeql-go@latest
 
 
 ### Resources:
-1. How to learn EdgeDB? [*here](https://docs.edgedb.com/tutorial)
-2. How to setup environment variables? [Recommended](https://docs.edgedb.com/database/reference/projects#ref-guide-using-projects) or [also here](https://docs.edgedb.com/libraries/connection) 
+1. How to learn EdgeDB? [*here](https://docs.edgedb.com/tutorial).
+2. How to setup environment variables? [Recommended](https://docs.edgedb.com/database/reference/projects#ref-guide-using-projects) or [also here](https://docs.edgedb.com/libraries/connection).
 
+Note:
+1. edgedb uses [edgeql](https://docs.edgedb.com/database/edgeql) as his main query language.
+2. Consider installing EdgeDB extension for your favorite code editor, mine is [Visual Studio Code](https://marketplace.visualstudio.com/items?itemName=magicstack.edgedb&ssr=false#review-details).
--- main.go
+++ main.go
@@ -2,28 +2,60 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/edgedb/edgedb-go"
 )
 
+type User struct {
+	ID   edgedb.UUID `edgedb:"id"`
+	Name string      `edgedb:"name"`
+	DOB  time.Time   `edgedb:"dob"`
+}
+
 func main() {
+	fmt.Println("program started")
+	opts := edgedb.Options{Concurrency: 4}
 	ctx := context.Background()
-	client, err := edgedb.CreateClient(ctx, edgedb.Options{})
+	db, err := edgedb.CreateClientDSN(ctx, "edgedb://edgedb@localhost/test", opts)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer client.Close()
+	defer db.Close()
 
-	var (
-		age   int64 = 21
-		users []struct {
-			ID   edgedb.UUID `edgedb:"id"`
-			Name string      `edgedb:"name"`
-		}
-	)
+	// create a user object type.
+	err = db.Execute(ctx, `
+        CREATE TYPE User {
+            CREATE REQUIRED PROPERTY name -> str;
+            CREATE PROPERTY dob -> datetime;
+        }
+    `)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	query := "SELECT User{name} FILTER .age = <int64>$0"
-	err = client.Query(ctx, query, &users, age)
+	// Insert a new user.
+	var inserted struct{ id edgedb.UUID }
+	err = db.QuerySingle(ctx, `
+        INSERT User {
+            name := <str>$0,
+            dob := <datetime>$1
+        }
+    `, &inserted, "Bob", time.Date(1984, 3, 1, 0, 0, 0, 0, time.UTC))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Select users.
+	var users []User
+	args := map[string]interface{}{"name": "Bob"}
+	query := "SELECT User {name, dob} FILTER .name = <str>$name"
+	err = db.Query(ctx, query, &users, args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(users)
 }
